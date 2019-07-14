bc2d7b98419fadd5356f75272a291a8ae3557cd9
gund3r <gund3r@inbox.ru>
2019-07-14 22:09:23 +0300

    Create field

--- src/Model/Field.java
+++ src/Model/Field.java
@@ -1,11 +1,23 @@
 package Model;
+import org.omg.CORBA.PRIVATE_MEMBER;
+
 import java.awt.Point;
 
 public class Field {
 
-    private int size;
+    private static final int FIELD_SIZE = 3;
 
-    public int getSize(){};
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
-    public Figure getFigure(Point point){}
+    public int getSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) {
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
 }
