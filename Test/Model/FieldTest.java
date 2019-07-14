5c988708a8199215050abc5dcc2f59c66477c2f8
gund3r <gund3r@inbox.ru>
2019-07-14 22:52:02 +0300

    Field chek has been added2

--- Test/Model/FieldTest.java
+++ Test/Model/FieldTest.java
@@ -1,5 +1,6 @@
 package Model;
 
+import Model.Exception.AlreadyOccupatedException;
 import Model.Exception.InvalidPointException;
 import org.junit.Test;
 
@@ -25,6 +26,19 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
+    }
+
+    @Test
+    public void testSetFigureWhenAlreadyOccupated() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupatedException e) {}
     }
 
     @Test
