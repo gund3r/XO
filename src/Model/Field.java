158692cd0f8dc0f87cea45971db0cc695aa06b35
gund3r <gund3r@inbox.ru>
2019-07-14 23:22:35 +0300

    Field refactoring

--- src/Model/Field.java
+++ src/Model/Field.java
@@ -1,4 +1,6 @@
 package Model;
+import Model.Exception.AlreadyOccupatedException;
+import Model.Exception.InvalidPointException;
 import org.omg.CORBA.PRIVATE_MEMBER;
 
 import java.awt.Point;
@@ -6,6 +8,9 @@ import java.awt.Point;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
+
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -13,11 +18,27 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
